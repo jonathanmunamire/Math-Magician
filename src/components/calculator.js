3cf8f9bb8ee9a21a6668584cad5c0d27310c0265
jonathanmunamire <jonathanmunamire@gmail.com>
2022-11-29 17:40:55 +0200

    Add Calculator Components

---
+++ src/components/calculator.js
@@ -0,0 +1,26 @@
+const Calculator = () => (
+  <div className="parent">
+    <div className="child output">0</div>
+    <button type="button" className="child">AC</button>
+    <button type="button" className="child">+/-</button>
+    <button type="button" className="child">%</button>
+    <button type="button" className="child orange">/</button>
+    <button type="button" className="child">7</button>
+    <button type="button" className="child">8</button>
+    <button type="button" className="child">9</button>
+    <button type="button" className="child orange">*</button>
+    <button type="button" className="child">4</button>
+    <button type="button" className="child">5</button>
+    <button type="button" className="child">6</button>
+    <button type="button" className="child orange">-</button>
+    <button type="button" className="child">1</button>
+    <button type="button" className="child">2</button>
+    <button type="button" className="child">3</button>
+    <button type="button" className="child orange">+</button>
+    <button type="button" className="child zero">0</button>
+    <button type="button" className="child">.</button>
+    <button type="button" className="child orange">=</button>
+  </div>
+);
+
+export default Calculator;
