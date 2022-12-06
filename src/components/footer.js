2955c0110987ad5f9b9c2ffe9610bea6c42a4369
jonathanmunamire <jonathanmunamire@gmail.com>
2022-12-06 20:21:08 +0200

    Add Footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import './footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer_text">
+      Copyright &copy; 2022 | Designed by&nbsp;
+      <span>Jonathan MUNAMIRE</span>
+    </p>
+  </footer>
+);
+
+export default Footer;
