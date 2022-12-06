63a8d27631298302754aabc1b5bd16582fa53812
jonathanmunamire <jonathanmunamire@gmail.com>
2022-12-06 19:51:45 +0200

    Style Calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -25,27 +25,29 @@ const Calculator = () => {
   return (
     <div className="calculator_page">
       <h2>Let&apos;s Do Some Math</h2>
-      <div className="parent">
-        <div className="child output">{result || 0}</div>
-        <button onClick={AddToObject} type="button" className="child">AC</button>
-        <button onClick={AddToObject} type="button" className="child">+/-</button>
-        <button onClick={AddToObject} type="button" className="child">%</button>
-        <button onClick={AddToObject} type="button" className="child orange">/</button>
-        <button onClick={AddToObject} type="button" className="child">7</button>
-        <button onClick={AddToObject} type="button" className="child">8</button>
-        <button onClick={AddToObject} type="button" className="child">9</button>
-        <button onClick={AddToObject} type="button" className="child orange">*</button>
-        <button onClick={AddToObject} type="button" className="child">4</button>
-        <button onClick={AddToObject} type="button" className="child">5</button>
-        <button onClick={AddToObject} type="button" className="child">6</button>
-        <button onClick={AddToObject} type="button" className="child orange">-</button>
-        <button onClick={AddToObject} type="button" className="child">1</button>
-        <button onClick={AddToObject} type="button" className="child">2</button>
-        <button onClick={AddToObject} type="button" className="child">3</button>
-        <button onClick={AddToObject} type="button" className="child orange">+</button>
-        <button onClick={AddToObject} type="button" className="child zero">0</button>
-        <button onClick={AddToObject} type="button" className="child">.</button>
-        <button onClick={AddToObject} type="button" className="child orange">=</button>
+      <div className="calculator">
+        <div className="parent">
+          <div className="child output">{result || 0}</div>
+          <button onClick={AddToObject} type="button" className="child ac">AC</button>
+          <button onClick={AddToObject} type="button" className="child">+/-</button>
+          <button onClick={AddToObject} type="button" className="child">%</button>
+          <button onClick={AddToObject} type="button" className="child">/</button>
+          <button onClick={AddToObject} type="button" className="child">7</button>
+          <button onClick={AddToObject} type="button" className="child">8</button>
+          <button onClick={AddToObject} type="button" className="child">9</button>
+          <button onClick={AddToObject} type="button" className="child">*</button>
+          <button onClick={AddToObject} type="button" className="child">4</button>
+          <button onClick={AddToObject} type="button" className="child">5</button>
+          <button onClick={AddToObject} type="button" className="child">6</button>
+          <button onClick={AddToObject} type="button" className="child">-</button>
+          <button onClick={AddToObject} type="button" className="child">1</button>
+          <button onClick={AddToObject} type="button" className="child">2</button>
+          <button onClick={AddToObject} type="button" className="child">3</button>
+          <button onClick={AddToObject} type="button" className="child plus">+</button>
+          <button onClick={AddToObject} type="button" className="child">.</button>
+          <button onClick={AddToObject} type="button" className="child">0</button>
+          <button onClick={AddToObject} type="button" className="child equal">=</button>
+        </div>
       </div>
     </div>
   );
